accdd8e577db6c1d03b8bb86f18eb8c62b81f411
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-12-10 02:00:31 +0100

    setup today date

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ const taskCountEl = document.getElementById('task-count');
 const filterButtons = document.querySelectorAll('.filter-btn');
 
 
+function loadTask(){
+    const saved = localStorage.getItem(STORAGE_KEY);
+    tasks = saved ? JSON.parse(saved) : [];
+}
+
+function saveTasks(){
+    localStorage.getItem(STORAGE_KEY, JSON.stringify(tasks));
+}
+
+function setTodayDate(){
+    const now = new Date();
+    const weekday = now.toLocaleDateString("en-US", {weekday: "short"});
+    const month = now.toLocaleDateString("en-US", {month: 'short'});
+    const day = String(now.getDate()).padStart(2, '0');
+    const year = now.getFullYear();
+    document.getElementById(
+        'date-display'
+    ).textContent = `${weekday} ${month} ${day} ${year}`;
+}
